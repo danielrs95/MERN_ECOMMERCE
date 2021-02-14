9f0de66610af514cc0f7129faec21086f0cdb42c
Daniel <danielrs9504@gmail.com>
2021-02-14 13:44:46 -0500

    Agrega descripcion al comentario

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,6 +4,7 @@ import { Card } from "react-bootstrap";
 // Como este componente está recibiendo el product del map que realizamos en el HomeScreen.js
 // Se podria usar const Product = (props) => {} ....
 // Pero usando corchetes es otra manera de acceder a lo que le envia el componente
+// Estamos usando deconstruccion!
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
