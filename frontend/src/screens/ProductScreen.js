a2634c09b134b9f184d10b6d4324d42c53378485
Daniel <danielrs9504@gmail.com>
2021-02-14 18:30:44 -0500

    Crea la relacion para seleccionar el producto de acuerdo al ID que se este enviando en la ruta

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+const ProductScreen = ({ match }) => {
+  // Estamos buscando en los productos, el producto cuyo ID coincida con el
+  // id enviado en la ruta en APP.js
+  // <Route path='/product/:id' component={ProductScreen} />
+  const product = products.find((p) => p._id === match.params.id);
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+    </>
+  );
 };
 
 export default ProductScreen;
