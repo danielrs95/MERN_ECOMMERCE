36794cb2ee129e3bcae87f9a223b37a911d232d3
Daniel <danielrs9504@gmail.com>
2021-02-17 19:52:27 -0500

    Agrega bcryp a las contraseñas de los usuarios creados

--- backend/data/user.js
+++ backend/data/user.js
@@ -2,17 +2,17 @@ const users = [
   {
     name: "Admin user",
     email: "admin@example.com",
-    password: xxx,
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
     name: "Daniel",
     email: "daniel@example.com",
-    password: xxx,
+    password: bcrypt.hashSync("123456", 10),
   },
   {
     name: "Albita",
     email: "albita@example.com",
-    password: xxx,
+    password: bcrypt.hashSync("123456", 10),
   },
 ];
