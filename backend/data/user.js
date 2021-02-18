7fbbc4ea2edda508508d5a1eb3aed3855de3c3d6
Daniel <danielrs9504@gmail.com>
2021-02-17 19:46:11 -0500

    elimina los _id de los productos y crea el archivo user.js con los usuarios de la aplicacion

---
+++ backend/data/user.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: "Admin user",
+    email: "admin@example.com",
+    password: xxx,
+    isAdmin: true,
+  },
+  {
+    name: "Daniel",
+    email: "daniel@example.com",
+    password: xxx,
+  },
+  {
+    name: "Albita",
+    email: "albita@example.com",
+    password: xxx,
+  },
+];
