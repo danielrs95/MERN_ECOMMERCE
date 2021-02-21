d88c056954b242856148925bcb8556faa19899ef
Daniel <danielrs9504@gmail.com>
2021-02-21 10:51:38 -0500

    crea el productReducer y crea el productListReducer encargado de manejar el estado de los productos que vemos en la pagina principal

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,24 @@
+export const productListReducer = (state = { products: [] }, action) => {
+  // De acuerdo a la acción que le pasemos actuará el reducer
+  switch (action.type) {
+    case "PRODUCT_LIST_REQUEST":
+      // Loading value, cuando hacemos el request, queremos que el
+      // componente sepa que estamos haciendo el fetch y retornamos
+      // los productos vacios, porque todavia no hemos hecho el fetch
+      return { loading: true, products: [] };
+
+    case "PRODUCT_LIST_SUCCESS":
+      // Loading false, porque ya hizo el request y
+      // products estara lleno con el objeto action que tendra un
+      // payload attached to it
+      return { loading: false, products: action.payload };
+
+    case "PRODUCT_LIST_FAIL":
+      // Loading false, ya cargo pero hay un error
+      // El error lo enviaremos en el payload
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
