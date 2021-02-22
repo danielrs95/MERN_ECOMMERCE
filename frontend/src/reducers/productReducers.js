b2a12b8ed1461668d6388cfb70d55dfc49c06d1a
Daniel <danielrs9504@gmail.com>
2021-02-22 09:46:16 -0500

    Crea el reducer, y el action para  las paginas en detalle de los productos

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -22,6 +25,26 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_FAIL:
       // Loading false, ya cargo pero hay un error
       // El error lo enviaremos en el payload
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const productDetailstReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      // Lo que sea que esté en el estado se lo pasaremos  al hacer el request
+      return { loading: true, ...state };
+
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
 
     default:
