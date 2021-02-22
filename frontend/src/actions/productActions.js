d8d46b7f66e6439cfd27f2a2256aed865f06d95a
Daniel <danielrs9504@gmail.com>
2021-02-22 10:03:12 -0500

    Setea las acciones en la vista ProductScreen para usar la informacion del estado

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -38,7 +38,7 @@ export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`/api/products${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data,
