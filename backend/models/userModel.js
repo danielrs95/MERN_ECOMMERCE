d46d0ad6b6e905350f7775c98f92f73bc7609793
Daniel <danielrs9504@gmail.com>
2021-02-23 19:06:11 -0500

    Crea la ruta para registrar usuarios

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,29 @@ const userSchema = mongoose.Schema(
   }
 );
 
+// compara la contra ingresada con el de la base de datos
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+/**
+ * Vamos a hacer segura la contraseña con bcrypt al momento
+ * De guardar un nuevo usuario
+ */
+
+userSchema.pre("save", async function (next) {
+  /**
+   * Si el campo no ha sido modificado! entonces next.
+   * Salimos del middleware
+   */
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Redefinimos la contraseña con ayuda de bcrypt
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
