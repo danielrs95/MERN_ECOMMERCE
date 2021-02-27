4ea2de1ef83a89cdd184b095861baa3f5584c720
Daniel <danielrs9504@gmail.com>
2021-02-27 11:35:20 -0500

    crea la ruta para actualizar el pago en la orden en el backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,7 +37,7 @@ const addOrderItems = AsyncHandler(async (req, res) => {
 });
 
 // @ desc   Get order by ID
-// @route   GET /api/order
+// @route   GET /api/orders
 // @access  Private
 const getOrderById = AsyncHandler(async (req, res) => {
   // Ademas de la orden queremos saber el nombre y email del usuario que hizo la orden
@@ -54,4 +54,29 @@ const getOrderById = AsyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @ desc   update order to paid
+// @route   GET /api/orders/:id
+// @access  Private
+const updateOrderToPaid = AsyncHandler(async (req, res) => {
+  // Ademas de la orden queremos saber el nombre y email del usuario que hizo la orden
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
