e3aa1b0ac5d5587c577820a875002fc5ec149de4
Daniel <danielrs9504@gmail.com>
2021-02-28 13:11:29 -0500

    Agrega la ruta de /api/users para admin y poder ver todos los usuarios de la aplicacion

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -107,4 +107,13 @@ const updateUserProfile = AsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+// @ desc   Get all user
+// @route   GET /api/users/
+// @access  Private/Admin
+const getUsers = AsyncHandler(async (req, res) => {
+  // get all users
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
